74b5e73288c5adea25474f450484f2277d17cc73
OSelymesi <77212643+OSelymesi@users.noreply.github.com>
2026-01-12 19:46:31 +0100

    Update benchmark.cu

--- benchmark.cu
+++ benchmark.cu
@@ -4,7 +4,7 @@
 #include <algorithm>
 #include <random>
 #include <cuda_runtime.h>
-#include "./include/usm_core.cuh" // Assuming usm_core.cuh is in the same directory or include path
+#include "./include/usm_core.cuh"
 
 // Macro for error handling
 #define CUDA_CHECK(call) { cudaError_t err = call; if(err != cudaSuccess) { printf("CUDA Error: %s line %d\n", cudaGetErrorString(err), __LINE__); exit(1); } }
@@ -209,4 +209,5 @@ int main() {
     
     return 0;
 }
+
 
